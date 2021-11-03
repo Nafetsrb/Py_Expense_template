9f7c51ced02ecd187c77dfcb012d5303d45dac15
Mateo Blagojevic <mateo@MacBook-Pro-de-Mateo.local>
2021-11-03 21:52:49 +0100

    Done some exercices

--- expense.py
+++ expense.py
@@ -1,6 +1,26 @@
 from PyInquirer import prompt
+import PyInquirer as inquirer
+from csv import writer
+from user import user_list, user_list_without_input
 
-expense_questions = [
+filename = "expense_report.csv"
+
+
+
+#Adding data to expense csv
+def csv_add(data):
+    to_add = []
+    with open(filename, "a+", newline='') as file:
+        csv_writer = writer(file)
+        for (key, val) in data.items():
+            to_add.append(val)
+        # Writing new expense in file
+        csv_writer.writerow(to_add)
+    return True
+
+def new_expense(*args):
+    #Updating the questions with new users
+    expense_questions = [
     {
         "type":"input",
         "name":"amount",
@@ -12,19 +32,39 @@ expense_questions = [
         "message":"New Expense - Label: ",
     },
     {
-        "type":"input",
+        "type":"list",
         "name":"spender",
-        "message":"New Expense - Spender: ",
+        "message":"Select a spender: ",
+        "choices": user_list()
     },
-
-]
-
-
-
-def new_expense(*args):
+    ]
     infos = prompt(expense_questions)
+    while (True):
+        answer = input("Did you split the amount of the bill? yes/no: ") 
+        if answer == "yes":
+            users = [{
+                "type":"checkbox",
+                "name": "refund",
+                "message":"Select users that needs to make a refund :",
+                "choices": user_list_without_input(infos.get('spender')),
+            },
+            ]
+            refund_users = prompt(users)
+            print(refund_users)
+            infos['refund'] = refund_users.get('refund')
+            infos['no_refund'] = []
+            print(infos)
+            break
+        if answer == "no":
+            infos['refund'] = []
+            infos['no_refund'] = []
+            break
+    isAdded = csv_add(infos)
+    if (isAdded):
+        print("New Expense Added")
+    else:
+        print("the Expense action failed!")
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
-    print("Expense Added !")
     return True
 
 
--- user.py
+++ user.py
@@ -1,8 +1,60 @@
 from PyInquirer import prompt
-user_questions = [
+from csv import writer
+from csv import reader
 
+filename = 'users.csv'
+user_questions = [    
+    {
+        "type":"input",
+        "name":"name",
+        "message":"Enter a unique username: ",
+    },
 ]
 
+#returns the list of users 
+def user_list():
+    users = []
+    with open(filename, "r") as file:
+        my_reader = reader(file, delimiter=',')
+        for row in my_reader:
+            users.append(row[0])
+    return users
+
+def user_list_without_input(user):
+    users = []
+    with open(filename, "r") as file:
+        my_reader = reader(file, delimiter=',')
+        for row in my_reader:
+            if (user != row[0]):
+                users.append({'name':row[0]})
+    return users
+            
+
+# Adding a new user only if it does not exist
+def csv_add(data):
+    to_add = []
+    with open(filename, "r") as file:
+        my_reader = reader(file, delimiter=',')
+        for row in my_reader:
+            for (key, value) in data.items():
+                if (value in row):
+                    print("Username is not unique! Try again")
+                    return False
+    with open(filename, "a+", newline='') as file:
+        csv_writer = writer(file)
+        for (key, val) in data.items():
+            to_add.append(val)
+        # Writing new expense in file
+        csv_writer.writerow(to_add)
+    return True
+    
+
 def add_user():
+    isAdded = False
+    # Try to add a user while th ename is not unique
+    while (not isAdded):
+        infos = prompt(user_questions)
+        isAdded = csv_add(infos)
+    print("User added successfully!")
     # This function should create a new user, asking for its name
-    return
+    return True
